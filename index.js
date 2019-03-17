682fcafca72fb320f4846b2e1fe66799b349387a
Derek Morey <derek.o.morey@gmail.com>
2019-03-17 17:32:48 -0400

    Added prod/dev build scripts
    Prod and dev builds were added to the package.json with the live reload
    server disabled in the production build

--- index.js
+++ index.js
@@ -7,21 +7,33 @@ const collections = require('metalsmith-collections');
 const dateFormatter = require('metalsmith-date-formatter');
 const fingerprint = require('metalsmith-fingerprint-ignore');
 
+function skip(opts) {
+	if (!opts.test()) {
+		return opts.plugin(opts.opts);
+	}
+	return (files, metalsmith, done) => {
+		setImmediate(done);
+	};
+}
+
 Metalsmith(__dirname)
 .metadata({
 	bandName: 'The Avery Foxes',
-	navLinks: [
-	]
+	liveReload: process.env.NODE_ENV === 'development',
 })
 .source('src')
 .destination('dist')
 .clean(true)
-.use(watch({
-	paths: {
-		'${source}/**/*': '**/*',
-		'layouts/**/*': '**/*',
-	},
-	livereload: 8081,
+.use(skip({
+	test: () => process.env.NODE_ENV === 'production',
+	plugin: watch,
+	opts: {
+		paths: {
+			'${source}/**/*': '**/*',
+			'layouts/**/*': '**/*',
+		},
+		livereload: 8081,
+	}
 }))
 .use(inPlace())
 .use(collections({
